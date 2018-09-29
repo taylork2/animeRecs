44e6c339b15893a82cdfe8d2c066f211e3994617
Taylor Tu <tkt7@njit.edu>
2018-09-28 23:09:41 -0400

    Fixing tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { AnimeComponent } from './anime/anime.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnimeComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     HttpClientModule
   ],
-  providers: [ HttpClientModule ],
+  providers: [ HttpModule, HttpClientModule ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/series.service.spec.ts
+++ src/app/series.service.spec.ts
@@ -1,11 +1,13 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { SeriesService } from './series.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('SeriesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [SeriesService]
+      providers: [SeriesService, HttpClientModule],
+      imports: [HttpClientModule]
     });
   });
 
